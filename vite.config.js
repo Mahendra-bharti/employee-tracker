07a740cdead773c5b268bcae1479827a2edab5e2
Mahendra-bharti <mahendrabbharti2003@gmail.com>
2025-03-04 14:14:18 +0530

    Configured GitHub Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+
 export default defineConfig({
+  base: "/employee-tracker/", // Make sure this matches your GitHub repository name
   plugins: [react(),   tailwindcss()],
-})
+});
