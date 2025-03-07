7d9a6d8b29966b4532b3cfff7e5f938a114200d5
Mahendra-bharti <mahendrabbharti2003@gmail.com>
2025-03-07 14:12:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base: "/employee-tracker/", // Make sure this matches your GitHub repository name
+  base: "/employee-tracker/",
   plugins: [react(),   tailwindcss()],
 });
